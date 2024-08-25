f37c5c2e3357d57960dda7767deddf87285f9d66
kevin38645 <kevin510622@gmail.com>
2024-08-25 22:16:05 +0800

    Player animation applied.

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -7,6 +7,8 @@ public class Player : MonoBehaviour
     [SerializeField] private float moveSpeed = 20f;
     [SerializeField] private float rotateSpeed = 10f;
 
+    private bool isWalking;
+
     private void Update() {
         Vector2 inputVector = new Vector2(0, 0);
         if(Input.GetKey(KeyCode.W)) {
@@ -21,10 +23,13 @@ public class Player : MonoBehaviour
         if (Input.GetKey(KeyCode.D)) {
             inputVector.x = +1;
         }
+
         inputVector = inputVector.normalized;
+
         Vector3 moveDir = new Vector3(inputVector.x, 0f, inputVector.y);
         transform.position += moveDir * moveSpeed * Time.deltaTime;
 
+        isWalking = (moveDir != Vector3.zero); 
         //Way to look at one side
         //transform.forward = moveDir; //method 1 not rotate smoothly
 
@@ -34,5 +39,9 @@ public class Player : MonoBehaviour
         //      transform.forward similar to moveDir: is Vector3(0,0,0) value 0-1
         transform.forward = Vector3.Slerp(transform.forward, moveDir, Time.deltaTime * rotateSpeed);
 
+    }
+
+    public bool IsWalking() {
+        return isWalking;
     }
 }
